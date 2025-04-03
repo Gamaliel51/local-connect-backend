98820a3a2a212af4d8a555f1efa335969e9a8fb2
Gamaliel51 <miraclendukwe51@gmail.com>
2025-04-03 19:52:48 +0100

    remove defaults

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use('/cart', cartRoutes)
 
 sequelize.sync().then(() => {
     app.listen(settings.port, () => {
-        createSampleRecords()
+        // createSampleRecords()
         console.log(`Server running on port ${settings.port}`)
     });
 })
